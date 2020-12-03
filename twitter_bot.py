b0ade3b47639cdb3c7cd64246366454a67457975
fharune <66402295+fharune@users.noreply.github.com>
2020-12-04 00:02:36 +0900

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -12,12 +12,22 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 
-tweets = api.user_timeline(id = "id")
+users = ["id1", "id2"]
 
-for status in tweets:
-    if ((not status.retweeted) and ("RT @" not in status.text)) and ("@" not in status.text):
-        tweet_id = status.id
-        try:
-            api.retweet(tweet_id)
-        except Exception as e:
-            print(e)
+def get_timeline(name,since_id):
+    for status in api.user_timeline(id = name, since_id = 1329902993420873729):
+        if ((not status.retweeted) and ("RT @" not in status.text)) and ("@" not in status.text):
+                tweet_id = status.id
+                try:
+                    api.retweet(tweet_id)
+                except Exception as e:
+                    print(e)
+                    
+for i, user in enumerate(users):
+    get_timeline(user,i)
+
+
+if __name__ == '__main__':
+    while True:
+        time.sleep(20)
+        get_timeline(users,1329902993420873729)
