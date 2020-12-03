0241781b20e189add24b43d488ccf65990ee5fa4
fharune <66402295+fharune@users.noreply.github.com>
2020-12-03 15:41:46 +0900

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -2,17 +2,17 @@ import tweepy
 
 
 
-CONSUMER_KEY = "zfAnk9Uz9bEOhhvOWZw0jPOzv"
-CONSUMER_SECRET = "MAl36eU4AsdBupiGUP11KIvT4yGEQgrO4bEXekvqh4U5koenoO"
-ACCESS_TOKEN = "1334325540556132354-68zZdUJKJvjEjii8Lz8tgk79Lx4JfL"
-ACCESS_SECRET = "OxueSL7rN7nAz78gq9qPCnpbdWU6Ze0mUTLJBgGpl73am"
+CONSUMER_KEY = "CONSUMER_KEY"
+CONSUMER_SECRET = "CONSUMER_SECRET"
+ACCESS_TOKEN = "ACCESS_TOKEN"
+ACCESS_SECRET = "ACCESS_SECRET"
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 
-tweets = api.user_timeline(id = "printemps_317")
+tweets = api.user_timeline(id = "id")
 
 for status in tweets:
     if ((not status.retweeted) and ("RT @" not in status.text)) and ("@" not in status.text):
